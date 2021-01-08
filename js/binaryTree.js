2e6a71e622f6a9c34a6c39070c1686a8f7a3c810
JorgePiaggio <proto2345@protonmail.com>
2021-01-08 20:12:31 -0300

    valueIsPresentInTree

--- js/binaryTree.js
+++ js/binaryTree.js
@@ -8,68 +8,93 @@ function Node(value) {
 
 function BinarySearchTree() {
 
-  this.root = null;
+    this.root = null;
 
-  this.add = (int) => {
-    if(Number.isInteger(int)){
-       let newNode = new Node(int);
-      
-        if(this.root === null){
-          this.root = newNode;
-        }else{
-          let look = new Node();
-          look = this.root;
+    this.add = (int) => {
 
-          while(look != null){ //console.log(look);
-            if(int < look.value){
-              if(!look.left){
-                look.left = newNode;
-                return;
-              }
-              look = look.left;
-            }else if(int > look.value){
-              if(!look.right){
-                look.right = newNode;
-                return;
-              }
-              look = look.right;
-            }else{ //num already exists
-              return null;
+        if(Number.isInteger(int)){
+            let newNode = new Node(int);
+        
+            if(this.root === null){
+                this.root = newNode;
+            }else{
+                let look = new Node();
+                look = this.root;
+
+                while(look != null){ //console.log(look);
+                    if(int < look.value){
+                        if(!look.left){
+                            look.left = newNode;
+                            return;
+                        }
+                    look = look.left;
+                    }else if(int > look.value){
+                        if(!look.right){
+                            look.right = newNode;
+                            return;
+                        }
+                    look = look.right;
+                    }else{ //num already exists
+                        return null;
+                    }
+                }
+                look.value = int;
             }
-          }
-          look.value = int;
+        }else{
+            return null;
         }
-    }else{
-      return null;
     }
-  }
 
+    //min value in tree
+    this.findMin = () => {
+        if(this.root === null)
+            return null;
 
-  this.findMin = () => {
-    if(this.root === null)
-    return null;
+        let min = this.root;
 
-    let min = this.root;
-
-    while(min.left){
-      min = min.left;
+        while(min.left){
+            min = min.left;
+        }
+        return min.value;
     }
-    return min.value;
-  }
 
+    //max value in tree
+    this.findMax = () => {
+        if(this.root === null)
+            return null;
 
-   this.findMax = () => {
-    if(this.root === null)
-    return null;
+        let max = this.root;
 
-    let max = this.root;
-
-    while(max.right){
-      max = max.right;
+        while(max.right){
+            max = max.right;
+        }
+        return max.value;
     }
-    return max.value;
-  }
 
+    //check presence of value in tree
+    this.isPresent = (value) => {
+        if(!this.root)
+            return false;
+
+        if(this.root.value === value)
+            return true;
+
+        let aux = new Node();
+        aux = this.root;
+
+        while(aux){
+            if(value === aux.value)
+                return true;
+
+            if(value < aux.value){
+                aux = aux.left;
+            }else if(value > aux.value){
+                aux = aux.right;
+            }
+        }
+        return false;
+        
+    }
 
 }
 
