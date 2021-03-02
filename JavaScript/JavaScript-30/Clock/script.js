7105b41e129bd50d05f3585c013c19d7a1d6d865
JorgePiaggio <proto2345@protonmail.com>
2021-03-01 23:15:37 -0300

    digital clock

--- JavaScript/JavaScript-30/Clock/script.js
+++ JavaScript/JavaScript-30/Clock/script.js
@@ -1,10 +1,13 @@
 const hourHand = document.querySelector('.hour_hand');
 const minHand = document.querySelector('.min_hand');
 const secHand = document.querySelector('.sec_hand');
+const dayDiv = document.querySelector('.day');
+const digitalDiv = document.querySelector('.digital');
 
 function setDate(){
     const now = new Date();
 
+    // analog
     const minutes = now.getMinutes();
     const minutesDegrees = ((minutes / 60) * 360) + 90;
     minHand.style.transform = `rotate(${minutesDegrees}deg)`;
@@ -16,7 +19,21 @@ function setDate(){
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
     secHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    //digital
+    document.getElementById("digitalString").innerHTML = hour+":"+minutes+":"+seconds;  
+
 }
+function setDay(){
+    const now2 = new Date();
+    var options = { weekday: 'long'};
+    const day = new Intl.DateTimeFormat('en-US', options).format(now2);
+    document.getElementById("dayString").innerHTML = day;  
+    console.log(day);
+}
+
 setDate();
+setDay();
 setInterval(setDate, 1000) // run setDate every second
+setInterval(setDay, 1000) // run setDate every second
 
