6e16e0dd27c42b51bf19748c9ea9cb598ad894e8
JorgePiaggio <proto2345@protonmail.com>
2021-03-01 21:39:16 -0300

    js clock

---
+++ JavaScript/JavaScript-30/Clock/script.js
@@ -0,0 +1,22 @@
+const hourHand = document.querySelector('.hour_hand');
+const minHand = document.querySelector('.min_hand');
+const secHand = document.querySelector('.sec_hand');
+
+function setDate(){
+    const now = new Date();
+
+    const minutes = now.getMinutes();
+    const minutesDegrees = ((minutes / 60) * 360) + 90;
+    minHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+    const hour = now.getHours();
+    const hourDegrees = ((hour / 12) * 360) + ((minutes/60)*30) + 90;  //
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secHand.style.transform = `rotate(${secondsDegrees}deg)`;
+}
+setDate();
+setInterval(setDate, 1000) // run setDate every second
+
