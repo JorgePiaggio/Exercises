cdb1b1490d14c4f6fa083f75027eea16506d047c
JorgePiaggio <proto2345@protonmail.com>
2021-03-11 10:11:13 -0300

    heat checkpoint

--- JavaScript/heat/index.js
+++ JavaScript/heat/index.js
@@ -1,41 +1,19 @@
 function heat(){
-    dro();
+    travel();dro(); 
 }
 
-function travel(){
-    var canv = document.getElementById("canvas");
-    var br = document.getElementById("broccoli");
-    var dx = Math.floor(Math.random() * 650 - 300);
-    var dy = Math.floor(Math.random() * 650 - 300);
-    var deg = Math.floor(Math.random() * 360);
-    var sca = Math.random() * 1;
-    
-    if(br.x > canv.width - 150 || br.x < 100 ){ dx*= -1;}
-    if(br.y > canv.heigth - 150 || br.y < 0){ dy*= -1;}
-    dynamics.animate(br, {
-        translateY: dx,
-        translateX: dy,
-        rotateZ: deg,
-        scale: sca
-    }, {
-    type: dynamics.easeIn,
-    duration: 3555,
-    friction: 222,
-    complete: function() {
-        travel()
-    }
-    })
-}
+/* universe */
 
 function dro(){  
+
     var el = document.getElementById("universe")
     dynamics.animate(el, {
         rotateZ: 180
     }, {
     type: dynamics.linear,
-    duration: 1500,
+    duration: 8500,
     complete: function() {
-        dro2()
+        dro3()
     }
     })
 }
@@ -43,15 +21,17 @@ function dro(){
 function dro2(){    
     var el = document.getElementById("universe")
     dynamics.animate(el, {
-        rotateZ: 320
+        rotateZ: 230
     }, {
     type: dynamics.linear,
-    duration: 3500,
+    duration: 700,
     complete: function() {
-         dro3()
+         heat()
     }
     })
 }
+
+/* cat  -  why cant join these func? */
 
 function dro3(){    
     var el = document.getElementById("cat")
@@ -110,7 +90,82 @@ function dro6(){
     duration: 11,
     friction: 200,
     complete: function() {
-        heat()
+        dro2()
     }
     })
 }
+
+/* broccoli */
+
+function travel(){
+    var canv = document.getElementById("canvas");
+    var br = document.getElementById("broccoli");
+    var dx = Math.floor(Math.random() * 650 - 300);
+    var dy = Math.floor(Math.random() * 650 - 300);
+    var sca = Math.random() * 1;
+    
+    if(br.x > canv.width - 150 || br.x < 100 ){ dx*= -1;}
+    if(br.y > canv.heigth - 150 || br.y < 0){ dy*= -1;}
+    
+    dynamics.animate(br, {
+        translateY: dx,
+        translateX: dy,
+        scale: sca
+    }, {
+    type: dynamics.spring,
+    duration: 1260,
+    friction: 516,
+    frequency: 943,
+    anticipationSize: 580,
+    anticipationStrength: 36,
+    complete: function() {
+        travel2()
+    }
+    })
+}
+
+function travel2(){
+    var br = document.getElementById("broccoli");
+    var deg = Math.floor(Math.random() * 360);
+
+    dynamics.animate(br, {
+        rotateZ: deg
+    }, {
+    type: dynamics.linear,
+    duration: 1260,
+    friction: 301,
+    complete: function() {
+        travel()
+    }
+    })
+}
+
+/*
+function travel(){
+    var canv = document.getElementById("canvas");
+    var br = document.getElementById("broccoli");
+    var dx = Math.floor(Math.random() * 650 - 300);
+    var dy = Math.floor(Math.random() * 650 - 300);
+    var deg = Math.floor(Math.random() * 360);
+    var sca = Math.random() * 1;
+    
+    if(br.x > canv.width - 150 || br.x < 100 ){ dx*= -1;}
+    if(br.y > canv.heigth - 150 || br.y < 0){ dy*= -1;}
+    
+    dynamics.animate(br, {
+        translateY: dx,
+        translateX: dy,
+        rotateZ: deg,
+        scale: sca
+    }, {
+    type: dynamics.easeIn,
+    duration: 3555,
+    friction: 222,
+    complete: function() {
+        travel()
+    }
+    })
+}
+ */
+
+heat(); 
